66ec847ac86e089f3013f3e658d9c1d3d4922b2d
Viktar Makouski <mcviktor@tut.by>
2021-12-20 23:15:30 +0300

    Testing CI 2

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
 
 
-
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
