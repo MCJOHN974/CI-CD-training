708e42a41e24191e4e957a664f6219e92dc3a4d7
Viktar Makouski <mcviktor@tut.by>
2021-12-25 19:39:10 +0300

    Not echo telebot and update Readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # CI-CD-training
-Symple python telegram bot to practice in deploing in Amazon AWS and CI-CD
+Symple python telegram bot to practice in deploying in Amazon AWS and CI-CD
+
+
+# HSE
+Маковский Виктор Сергеевич, 202 группа. 
+
+Сделано:
+Код работающего не эхо бота
+
+Докерфайл, с помощью которого собирается правильно работающий контейнер
+
+Джоба в гитхабе, проверяющая pep8 при коммите
+
+Запущен сервер на Amazon AWS на котором можно ручками сделать ```git pull```
+
--- main.py
+++ main.py
@@ -1,27 +1,57 @@
-import sys
-
 import telebot
 import datetime
 import os
+import random
 
 
 API_TOKEN = os.environ.get('TELEBOT_TOKEN')
 bot = telebot.TeleBot(API_TOKEN)
 
+aneks = []
+with open('data.txt') as file:
+    aneks = file.read().split("===")
+start_message = ''
+
+with open('start_message.txt') as file:
+    start_message = file.read()
+
+
+to_add = set()
+
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+    bot.reply_to(message, start_message)
+
+
+@bot.message_handler(commands=['anek'])
+def echo_all(message):
+    bot.reply_to(message, aneks[random.randint(0, len(aneks)) - 1])
+    # logging
+    print(datetime.datetime.now(), message.from_user.id, (message.text + " " * 20)[:20],
+          message.from_user.username, sep='\t')
+
+
+@bot.message_handler(commands=['add'])
+def echo_all(message):
+    bot.reply_to(message, 'Отправь свой анек.')
+    to_add.add(message.from_user.id)
+    # logging
+    print(datetime.datetime.now(), message.from_user.id, (message.text + " " * 20)[:20],
+          message.from_user.username, sep='\t')
 
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    bot.reply_to(message, message.text)
-    if message.text == API_TOKEN:
-        sys.exit("SIGINT from telegram messages")
-    # logging
-    print(datetime.datetime.now(), message.from_user.id, (message.text + " " * 20)[:20],
-          message.from_user.username, sep='\t')
+    if message.from_user.id in to_add:
+        aneks.append(message.text)
+        to_add.remove(message.from_user.id)
+        bot.reply_to(message, 'Анек успешно добавлен.')
+    else:
+        bot.reply_to(message, 'Нет такой команды.')
+        # logging
+        print(datetime.datetime.now(), message.from_user.id, (message.text + " " * 20)[:20],
+              message.from_user.username, sep='\t')
 
 
 bot.infinity_polling()
